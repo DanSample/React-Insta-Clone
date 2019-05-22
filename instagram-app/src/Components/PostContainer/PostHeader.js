1c2bb6b21031c2bc8c36bab19270252b0a7a40a1
Dan Sample <danssample@gmail.com>
2019-05-22 16:28:15 -0700

    built out PostHeader component

--- instagram-app/src/Components/PostContainer/PostHeader.js
+++ instagram-app/src/Components/PostContainer/PostHeader.js
@@ -1,3 +1,17 @@
 import React from 'react';
 
+const PostHeader = props => {
+  return (
+    <div className="post-header">
+      <div className="post-thumb-wrapper">
+        <img
+          className="post-thumb"
+          alt="post thumbnail"
+          src={props.thumbnailUrl}
+        />
+      </div>
+      <div>{props.username}</div>
+    </div>
+  );
+};
 export default PostHeader;
