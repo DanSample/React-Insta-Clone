fc7e9bbaeb8b4c65fcc9f9d34aa5c7b3c138adbb
Dan Sample <danssample@gmail.com>
2019-05-22 18:33:56 -0700

    styled postContainer and a few adjustments in index.css

--- instagram-app/src/Components/SearchBar/SearchBar.js
+++ instagram-app/src/Components/SearchBar/SearchBar.js
@@ -5,20 +5,20 @@ const SearchBar = () => {
   return (
     <div className="search-bar-wrapper">
       <div className="image-wrapper">
-        <div className="fab fa-instagram fa-3x" />
+        <div className="fab fa-instagram fa-2x" />
       </div>
       <div className="input-bar">
         <input type="text" placeholder="Search" />
       </div>
       <div className="social-wrapper">
         <div className="social">
-          <i className="fa fa-compass fa-3x" />
+          <i className="fa fa-compass fa-2x" />
         </div>
         <div className="social">
-          <i className="fa fa-heart fa-3x" />
+          <i className="fa fa-heart fa-2x" />
         </div>
         <div className="social">
-          <i className="fa fa-user-circle fa-3x" />
+          <i className="fa fa-user-circle fa-2x" />
         </div>
       </div>
     </div>
