61a27d54cb59849d498a9e38b61c255fed48a5fd
Dan Sample <danssample@gmail.com>
2019-05-30 20:40:02 -0700

    File Add: LoginPage.js with basic Login class constructor built

---
+++ instagram-app/src/Components/Login/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoginPage.css';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+}
